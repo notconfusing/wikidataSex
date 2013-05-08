a431f59f6bd1fc3bbace08786e8300fc1dcb1e4d
notconfusing <isalix@gmail.com>
2013-05-08 10:51:35 -0700

    actual stability improvements

--- wdsex.py
+++ wdsex.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-#import os
-#os.environ['PYWIKIBOT2_DIR'] = '/home/notconfusing/workspace/gender_wd/rewrite/pywikibot'
+import os
+os.environ['PYWIKIBOT2_DIR'] = '/home/kleinm/workspace/WikidataVIAFbot_branch'
 import pywikibot
 from collections import defaultdict
 import urllib2
@@ -10,6 +10,8 @@ import xml
 import xml.etree.ElementTree as ET
 import rdflib
 import json
+import logging
+logging.basicConfig(filename='logofwdsex.log',level=logging.DEBUG)
 
 #some constants
 CORE = rdflib.Namespace("http://www.w3.org/2004/02/skos/core#")
@@ -36,20 +38,22 @@ propertyMap = {'LCCN': 'P244',
              'sex': 'P21',
              'imported from':'P143'}
 
-sexItemMap = {'Q6581097': 'male', 
+ItemMap = {'Q6581097': 'male', 
                'Q6581072': 'female', 
                'Q1097630': 'intersex'}
 
 sexMap = {'female': 'Q6581072',
          'male': 'Q6581097',
-         'intersex' : 'Q1097630'}
+         'intersex' : 'Q1097630',
+         'VIAF': 'Q54919',
+         'LCCN': 'Q620946',}
 
 #make the wikidatapagegenerators
 sex_property_page = pywikibot.ItemPage(wikidata, 'Property:' + propertyMap['sex'])
 viaf_property_page = pywikibot.ItemPage(wikidata, 'Property:' + propertyMap['VIAF'])
 
 pages_with_sex = sex_property_page.getReferences(total=10000)
-pages_with_viaf = viaf_property_page.getReferences(total=1000)
+pages_with_viaf = viaf_property_page.getReferences(total=1000000)
 
 #cases of interest
 try:
@@ -58,7 +62,7 @@ try:
     casesJSON.close()
 except IOError:
     cases = {'prevtouched' : 0, 'seen' : 0, 'lccnnuances': [], 
-             'wdnotviaf': 0, 'viafnotwd': 0, 'wdviafagree': 0, 'wdviafdisagree': 0, 
+             'wdnotviaf': 0, 'viafnotwd': 0, 'wdviafagree': 0, 'wdviafdisagree': [], 
              'viafmale': 0, 'viaffemale': 0, 'wdmale':0, 'wdfemale' : 0, 'wdintersex': 0 }
     
 def savecases():
@@ -110,11 +114,10 @@ def getLCCNOpinion(LCCNLink):
 
 def getVIAFOpinionFromXML(marcxmlurl):
     '''Returns a sex, or none if none exists'''
-    marc_xml_string = urllib2.urlopen(marcxmlurl).read()
     try:
+        marc_xml_string = urllib2.urlopen(marcxmlurl).read()
         root = ET.fromstring(marc_xml_string)
-    except ET.ParseError:
-        print marc_xml_string
+    except (ET.ParseError, urllib2.HTTPError, urllib2.URLError) as e:
         raise noVIAFOpinion
     ET.register_namespace('http://www.loc.gov/MARC21/slim','')
     for child in root:
@@ -124,53 +127,106 @@ def getVIAFOpinionFromXML(marcxmlurl):
             if datafield_attrib['tag'] == '375':
                 for subfield in datafield:
                     if subfield.attrib['code'] == 'a':
-                        return subfield.text
+                        if subfield.text == 'unknown':
+                            return None
+                        else:
+                            return subfield.text
     #nothing was found
     raise noVIAFOpinion
 
 
 def getviafOpinion(viafnum):
-    marcxmlurl = 'http://viaf.org/viaf/'+viafnum+'/marc21.xml'
-    try:
-        viafOpinion = getVIAFOpinionFromXML(marcxmlurl)
-    except noVIAFOpinion:
-        viafOpinion = None
+    viafOpinion = None
+    if viafnum:
+        marcxmlurl = 'http://viaf.org/viaf/'+viafnum+'/marc21.xml'
+        try:
+            viafOpinion = getVIAFOpinionFromXML(marcxmlurl)
+        except noVIAFOpinion:
+            return viafOpinion
     return viafOpinion
 
 def getlccnOpinion(viafnum):
-    xmlurl = 'http://viaf.org/viaf'+viafnum +'/rdf.xml'
-    rdfgraph=rdflib.Graph()
-    try:
-        rdfgraph.load(xmlurl)
-    except (xml.sax._exceptions.SAXParseException, urllib2.HTTPError) as e:
-        print 'xml or url error'
-        print e
-        lccnOpinion = None
-        return lccnOpinion
-    try: 
-        LCCNLink = findLCCNLink(rdfgraph)
-        lccnOpinion = getLCCNOpinion(LCCNLink)
-    except (noLCCNLink, noLCCNOpinion) as e:
-        lccnOpinion = None
-        return lccnOpinion
+    lccnOpinion = None
+    if viafnum:
+        xmlurl = 'http://viaf.org/viaf/'+viafnum +'/rdf.xml'
+        rdfgraph=rdflib.Graph()
+        try:
+            rdfgraph.load(xmlurl)
+        except (xml.sax._exceptions.SAXParseException, urllib2.HTTPError) as e:
+            print 'xml or url error'
+            print e
+            return lccnOpinion
+        try: 
+            LCCNLink = findLCCNLink(rdfgraph)
+            lccnOpinion = getLCCNOpinion(LCCNLink)
+        except (noLCCNLink, noLCCNOpinion, urllib2.HTTPError) as e:
+            return lccnOpinion
     
     return lccnOpinion
 
 
-def mostInformative(viafOpinions):
+def mostInformativeVIAF(viafOpinions, page):
     '''takes a list of viafOpinions. 
-    returns a viafOpinion, which has more sex data than the other, or the first one if both equal'''
-    return viafOpinion
+    returns a viafOpinion, which has more sex data than the other'''
+    mostInformativeOpinion = None
+    for viafOpinion in viafOpinions:
+        #is there an opinion here?
+        if viafOpinion:
+            #is there a potentially competing opinion?
+            if not mostInformativeOpinion:
+                mostInformativeOpinion = viafOpinion
+            #if there is already an opinion is it the same?
+            elif mostInformativeOpinion == viafOpinion:
+                continue
+            else:
+                #conflicting viaf, that's weird, lets log it
+                logging.warning('conflicting sex for multiple viafs on %s', page.title())
+                continue
+    return mostInformativeOpinion
 
-def determineCase(pageTitle, wikidataOpinion, lccnOpinion, viafOpinion):
+def mostInformativeLCCN(lccnOpinions, page):
+    '''takes a list of lccnOpinions. 
+    returns a viafOpinion, which has more sex data than the other, or the first one if both equal'''
+    mostInformativeOpinion = None
+    greatestLength = 0
+    for lccnOpinion in lccnOpinions:
+        if lccnOpinion:
+            if len(lccnOpinion) > greatestLength:
+                mostInformativeOpinion = lccnOpinion
+    return mostInformativeOpinion
+
+def addClaimWithSource(page, opinion):
+    claimObj = pywikibot.Claim(site=wikidata, pid=propertyMap['sex'])
+    sexPageValue = pywikibot.ItemPage(site=wikidata, title=sexMap[opinion])
+    claimObj.setTarget(sexPageValue)
+    
+    page.addClaim(claimObj)
+    print 'omg added'
+    
+    sourceObj = pywikibot.Claim(site=wikidata, pid=propertyMap['imported from'])
+    VIAFPageValue = pywikibot.ItemPage(site=wikidata, title=sexMap['VIAF'])
+    sourceObj.setTarget(VIAFPageValue)
+    
+    claimObj.addSource(sourceObj, bot=True)
+
+def addSourceToClaim(claim):
+    
+    sourceObj = pywikibot.Claim(site=wikidata, pid=propertyMap['imported from'])
+    VIAFPageValue = pywikibot.ItemPage(site=wikidata, title=sexMap['VIAF'])
+    sourceObj.setTarget(VIAFPageValue)
+    
+    claim.addSource(sourceObj, bot=True)
+    print 'omg added'
+        
+
+def determineCase(page, sexClaim, wikidataOpinion, lccnOpinion, viafOpinion):
     cases['seen'] += 1
-    #normalize viaf
-    if viafOpinion == 'unknown': 
-        viafOpinion = None
     #does lccn have transitioning dates
     if lccnOpinion:
         if len(lccnOpinion) > 1 or len(lccnOpinion[0].keys()) > 1:
-            cases['lccnnuances'].append(pageTitle)
+            cases['lccnnuances'].append(page.title())
+            #we'er just going to log the complex cases for now, until we can add with qualifiers
+            return
     if wikidataOpinion:
         wdkey = 'wd' + wikidataOpinion
         cases[wdkey] += 1
@@ -181,21 +237,27 @@ def determineCase(pageTitle, wikidataOpinion, lccnOpinion, viafOpinion):
         cases[viafkey] += 1
         if not wikidataOpinion:
             cases['viafnotwd'] += 1
+            #we're adding new data
+            #addClaimWithSource(page, viafOpinion)
     if wikidataOpinion and viafOpinion:
         if wikidataOpinion == viafOpinion:
             cases['wdviafagree'] += 1
+            #addSourceToClaim(sexClaim)
         else:
-            cases['wdviafdisagree'] += 1
+            cases['wdviafdisagree'].append(page.title())
+            #addClaimWithSource(viafOpinion)
 
 def genSexData():
-    seen = 0
+    touched = 0
     for page in pages_with_viaf:
-        seen += 1
-        if cases['prevtouched'] >= seen:
+        touched += 1
+        print touched
+        if cases['prevtouched'] >= touched:
             continue
         wikidataOpinion = None
         # a list of result for possibly many viaf nums whic we'll later pick one of.
         viafnums = list()
+        sexClaim = None
         print 'page: ', page 
         try:
             page_parts = page.get()
@@ -206,20 +268,30 @@ def genSexData():
             for claim in claim_list:
                 if claim.id == 'p214':
                     viafnums.append(claim.target)
+                    print 'viafvalue is: ',  claim.target
+                    print type(claim.target)
                 if claim.id == 'p21':
                     #under the current implementation sex is supposed to be single-valued, i'm actually upset about this and wrote so on the talk page, but until then, sigh.
                     if not wikidataOpinion:
-                        wikidataOpinion = sexItemDict[claim.target.title()]
+                        #some sex values are unkown, like for jack the ripper
+                        if claim.target:
+                            wikidataOpinion = ItemMap[claim.target.title()]
+                            sexClaim = claim
                     #what if there's a better one that has a source
                     elif claim.sources:
-                        wikidataOpinion = sexItemDict[claim.target.title()]
+                        if claim.target:
+                            wikidataOpinion = ItemMap[claim.target.title()]
+                            sexClaim = claim
+        print 'wikidataopinion: ', wikidataOpinion
         lccnOpinions = [getlccnOpinion(viafnum) for viafnum in viafnums]
+        lccnOpinion = mostInformativeLCCN(lccnOpinions, page)
+        print 'lccnopinions: ',  lccnOpinions, 'most informative: ', lccnOpinion
         viafOpinions = [getviafOpinion(viafnum) for viafnum in viafnums]
-        lccnOpinion = mostInformative(lccnOpinions)
-        viafOpinion = mostInformative(viafOpinons)                                 
-        determineCase(page.title(), wikidataOpinion, lccnOpinion, viafOpinion) 
+        viafOpinion = mostInformativeVIAF(viafOpinions, page)        
+        print 'viafopinions: ',  viafOpinions, 'most informative: ', viafOpinion                  
+        determineCase(page, sexClaim, wikidataOpinion, lccnOpinion, viafOpinion)
         #print ' wd: ', wikidataOpinion, ' lc: ', lccnOpinion, ' viaf: ', viafOpinion
-        cases['prevtouched'] = seen
+        cases['prevtouched'] = touched
         savecases()
 
 genSexData()
